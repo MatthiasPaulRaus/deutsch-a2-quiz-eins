2eccc313a7a28130e2df0888a46e3820d13f206f
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-07-08 12:15:45 +0200

    first commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Was macht ihr mit euren Kindern am Wochenende?",
+        answersList: [
+            {answer:"Wir fahren zusammen in die Berge.", isCorrect:true},
+            {answer:"Ich möchte meinen Onkel besuchen.", isCorrect:false},
+            {answer:"Mal sehen, ob sie meine Tante anrufen.", isCorrect:false},
+        ]
+    },{
+        question:"Wohin sollen wir das Bild hängen?",
+        answersList: [
+            {answer:"Ich würde es in den Schrank stellen.", isCorrect:false},
+            {answer:"Hm, am besten neben das Regal.", isCorrect:true},
+            {answer:"Keine Ahnung, vielleicht in der Ecke.", isCorrect:false},
+        ]
+    },{
+        question:"Hast du meine Brille gesehen?",
+        answersList: [
+            {answer:"Schau mal, der Schrank.", isCorrect:false},
+            {answer:"Tut mir leid, aber ich habe keine.", isCorrect:false},
+            {answer:"Ja, ich glaube sie liegt auf dem Tisch.", isCorrect:true},
+        ]
+    },{
+        question:"Entschuldigung, wo ist das Obst?",
+        answersList: [
+            {answer:"Ich habe es in den Kühlschrank gelegt.", isCorrect:true},
+            {answer:"Da ist noch Gemüse in der Tasche.", isCorrect:false},
+            {answer:"Er steht in der Küche.", isCorrect:false},
+        ]
+    },{
+        question:"Was möchtet ihr denn im Urlaub machen?",
+        answersList: [
+            {answer:"Wir wollen auf einem Bauernhof leben.", isCorrect:false},
+            {answer:"Vielleicht eine Reise nach Italien.", isCorrect:true},
+            {answer:"Eine Radtour durch die Stadt.", isCorrect:false},
+        ]
+    },{
+        question:"Ich würde gern einen Kochkurs machen. Und du?",
+        answersList: [
+            {answer:"Er würde lieber einen Kletterkurs machen.", isCorrect:false},
+            {answer:"Sie interessiert sich für Fotografie.", isCorrect:false},
+            {answer:"Kochen mag ich nicht. Tanzen ist mir lieber.", isCorrect:true},
+        ]
+    },{
+        question:"Wissen Sie, wo man einen Fotokurs machen kann?",
+        answersList: [
+            {answer:"Bestimmt gibt es einen im Kulturzentrum.", isCorrect:true},
+            {answer:"Ja, dazu musst du an den Strand fahren.", isCorrect:false},
+            {answer:"Das kannst du nur in den Bergen machen.", isCorrect:false},
+        ]
+    },{
+        question:"Welches T-Shirt gefällt dir am besten?",
+        answersList: [
+            {answer:"Das mit dem Metallica-Logo finde ich gut.", isCorrect:false},
+            {answer:"Das mit dem Beatles-Foto finde ich am schönsten.", isCorrect:true},
+            {answer:"Das mit dem Alpen-Foto gefällt mir auch.", isCorrect:false},
+        ]
+    },{
+        question:"Wie lange dauert das Tennisturnier?",
+        answersList: [
+            {answer:"Im nächsten Monat.", isCorrect:false},
+            {answer:"Ab Anfang Mai.", isCorrect:false},
+            {answer:"Vom 14. bis zum 16.Juni.", isCorrect:true},
+        ]
+    },{
+        question:"Seit wann ist das Fitnesscenter geöffnet?",
+        answersList: [
+            {answer:"Schon seit über einem Monat.", isCorrect:true},
+            {answer:"Ich glaube, nächsten Monat.", isCorrect:false},
+            {answer:"Ungefähr bis Ende des Monats.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Haben Sie Lust, das Konzert zu besuchen?",
+        answersList: [
+            {answer:"Ja gern, aber am Wochenende haben wir keine Zeit.", isCorrect:true},
+            {answer:"Nein, er mag keine Konzerte.", isCorrect:false},
+            {answer:"Ja, sie haben Lust auf ein Konzert.", isCorrect:false},
+        ]
+    },{
+        question:"Sagen Sie mal, ist das Schwimmbad immer so voll?",
+        answersList: [
+            {answer:"Schade, dass Sie nicht mitkommen.", isCorrect:false},
+            {answer:"Ja, gehen wir lieber in den Park.", isCorrect:true},
+            {answer:"Ich hoffe, dass Sie schwimmen können.", isCorrect:false},
+        ]
+    },{
+        question:"Was machst du in deiner Freizeit?",
+        answersList: [
+            {answer:"Sie treffen ihre Freunde, wenn sie Zeit haben.", isCorrect:false},
+            {answer:"Wenn ich meine Freunde treffe, habe ich Freizeit.", isCorrect:false},
+            {answer:"Wenn ich Zeit habe, treffe ich meine Freunde.", isCorrect:true},
+        ]
+    },{
+        question:"Entschuldigung, können Sie mir helfen?",
+        answersList: [
+            {answer:"Ja, ich helfe Ihnen gern.", isCorrect:true},
+            {answer:"Ja, sie hilft Ihnen gern.", isCorrect:false},
+            {answer:"Ja, ihr helft uns gern.", isCorrect:false},
+        ]
+    },{
+        question:"Hast du meinen Schlüssel gesehen?",
+        answersList: [
+            {answer:"Nein, er hat ihn nicht gesehen.", isCorrect:false},
+            {answer:"Ja, schau mal, hier liegt er.", isCorrect:true},
+            {answer:"Ja, er gibt ihn dir gleich.", isCorrect:false},
+        ]
+    },{
+        question:"Wie war der Film?",
+        answersList: [
+            {answer:"Sehr spannend. Den kann ich dir wirklich empfehlen.", isCorrect:false},
+            {answer:"Schlecht. Den können Sie nicht empfehlen.", isCorrect:false},
+            {answer:"Er hat ihnen gar nicht gefallen.", isCorrect:true},
+        ]
+    },{
+        question:"Es ist schon halb acht!",
+        answersList: [
+            {answer:"Wissen Sie, ob das Geschäft noch offen ist?", isCorrect:true},
+            {answer:"Halb acht ist mir zu früh.", isCorrect:false},
+            {answer:"Ich weiß nicht, ob heute geöffnet ist.", isCorrect:false},
+        ]
+    },{
+        question:"Wie wird das Wetter am Wochenende?",
+        answersList: [
+            {answer:"Gestern war es sehr schlecht.", isCorrect:false},
+            {answer:"Ich weiß nicht, wie es sein wird.", isCorrect:true},
+            {answer:"Ich weiß nicht, wann es regnet.", isCorrect:false},
+        ]
+    },{
+        question:"Wo seid ihr am liebsten im Urlaub?",
+        answersList: [
+            {answer:"Mein Traum ist ein Wochenende in Paris.", isCorrect:false},
+            {answer:"Leider hatten wir keinen Urlaub.", isCorrect:false},
+            {answer:"Am Strand in Spanien.", isCorrect:true},
+        ]
+    },{
+        question:"Waren Sie schon einmal auf dieser Insel?",
+        answersList: [
+            {answer:"Ja, wir waren schon zweimal da.", isCorrect:true},
+            {answer:"Nein, sie war noch nie da.", isCorrect:false},
+            {answer:"Ja, er war im letzten Sommer dort.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Auf wen freut ihr euch am meisten?",
+        answersList: [
+            {answer:"Wir freuen uns sehr auf unseren Onkel.", isCorrect:true},
+            {answer:"Auf Weihnachten.", isCorrect:false},
+            {answer:"Morgen kommt meine Tante zu uns.", isCorrect:false},
+        ]
+    },{
+        question:"Wann sind die Weihnachtsferien?",
+        answersList: [
+            {answer:"Entweder im Dezember oder im Januar.", isCorrect:false},
+            {answer:"Ende Dezember bis Anfang Januar.", isCorrect:true},
+            {answer:"Von Dezember bis Januar.", isCorrect:false},
+        ]
+    },{
+        question:"Woher kommt ihr so spät?",
+        answersList: [
+            {answer:"Vom Einkaufen.", isCorrect:false},
+            {answer:"Zum Einkaufen.", isCorrect:false},
+            {answer:"Ja, es ist schon sehr spät.", isCorrect:true},
+        ]
+    },{
+        question:"Wohin gehst du nach der Arbeit?",
+        answersList: [
+            {answer:"Hm, ich gehe noch zum Einkaufen.", isCorrect:true},
+            {answer:"Sie muss noch zum Arzt gehen.", isCorrect:false},
+            {answer:"Meine Freunde gehen zum Sportplatz.", isCorrect:false},
+        ]
+    },{
+        question:"Hast du Lust auf einen Spaziergang?",
+        answersList: [
+            {answer:"Nein, das war meine Idee.", isCorrect:false},
+            {answer:"Ja, aber das ist nicht sehr praktisch.", isCorrect:false},
+            {answer:"Ja, das ist eine gute Idee.", isCorrect:true},
+        ]
+    },{
+        question:"Durftest du als Kind am Abend lange fernsehen?",
+        answersList: [
+            {answer:"Nein, ich musste schon früh ins Bett gehen.", isCorrect:true},
+            {answer:"Ich wollte immer lange fernsehen.", isCorrect:false},
+            {answer:"Ich wollte immer früh ins Bett gehen.", isCorrect:false},
+        ]
+    },{
+        question:"Von wem hast du das Buch bekommen?",
+        answersList: [
+            {answer:"Das hat die Kollegin leider vergessen.", isCorrect:false},
+            {answer:"Ich glaube, das ist von meiner Kollegin.", isCorrect:true},
+            {answer:"Meine Kollegin hat es ihr geschenkt.", isCorrect:false},
+        ]
+    },{
+        question:"Was machen Sie mit dem alten Fahrrad?",
+        answersList: [
+            {answer:"Ich will ein neues kaufen.", isCorrect:false},
+            {answer:"Er will eine Radtour machen.", isCorrect:false},
+            {answer:"Ich werde es meiner Schwester schenken.", isCorrect:true},
+        ]
+    },{
+        question:"Wissen Sie, wer zum Seminar kommt?",
+        answersList: [
+            {answer:"Ich glaube, dass fast alle kommen.", isCorrect:true},
+            {answer:"Viele Kollegen wollen mitkommen.", isCorrect:false},
+            {answer:"Ein paar Kollegen konnten nicht kommen.", isCorrect:false},
+        ]
+    },{
+        question:"Wissen Sie, ob man Sportschuhe tragen muss?",
+        answersList: [
+            {answer:"Die Reiseleiterin trägt keine Sportschuhe.", isCorrect:false},
+            {answer:"Viele Leute tragen leichte Schuhe.", isCorrect:false},
+            {answer:"Fragen Sie am besten den Reiseleiter.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
